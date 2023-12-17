a4d518ef6ef0c6163cea0e7591a3067d1572b6df
Filip-Fedor <118117604+Filip-Fedor@users.noreply.github.com>
2023-12-17 14:02:18 +0100

    rollback

--- Project5/stack.h
+++ Project5/stack.h
@@ -6,187 +6,180 @@
 #include <map>
 #include <memory>
 #include <set>
+#include <iostream>
 
 template<typename K, typename V>
-class Storage {
+class Element {
 private:
-	const K key;
-	size_t first_referenced = 0;
-	std::stack<std::pair<V, size_t>> values;
+	std::shared_ptr<K> key_ptr;
+	std::shared_ptr<V> value_ptr;
+	size_t height = 0;
 
 public:
-	Storage(const K &_key) : key(_key) {}
+	Element (std::shared_ptr<K> _key_ptr, 
+			std::shared_ptr<V> _value_ptr, size_t _height)
+		: key_ptr(_key_ptr), value_ptr(_value_ptr), height(_height) {}
 
-	Storage(const std::shared_ptr<Storage<K, V>> &pointer) 
-		: key(pointer->key)
-		, first_referenced(0)
-		, values(pointer->values) {}
-
-	void push(const V &value, size_t height) {
-		values.push(std::pair(value, height));
-
-		if (first_referenced > 0)
-			first_referenced++;
-	}
-
-	void pop() {
-		values.pop();
-
-		if (first_referenced > 0)
-			first_referenced--;
-	}
-
-	V & top() {
-		V &top = values.top().first;
-		
-		if (first_referenced == 0)
-			first_referenced = 1;
-
-		return top;
-	}
-
-	V const & top() const {
-		V const &top = values.top().first;
-		return top;
+	std::shared_ptr<K> key_pointer() const {
+		return key_ptr;
 	}
 
 	K const & get_key() const {
-		return key;
+		return *key_ptr;
 	}
 
-	size_t size() const {
-		return values.size();
+	V const & get_value() const {
+		return *value_ptr;
+	}
+	
+	V & get_value() {
+		return *value_ptr;
+	}
+	
+	std::shared_ptr<V> value_pointer() const {
+		return value_ptr;
 	}
 
-	bool empty() const {
-		return size() == 0;
+	size_t get_height() const {
+		return height;
 	}
+};
 
-	bool is_referenced() const {
-		return first_referenced > 0;
+struct key_comparator {
+	template<typename K>
+	bool operator()(const std::shared_ptr<K> &a, 
+					const std::shared_ptr<K> &b) const {
+
+		return *a < *b;
 	}
+};
 
-	size_t height() const {
-		if (empty())
-			return 0;
-		return values.top().second;
+struct element_height_comparator {
+	template<typename K, typename V>
+	bool operator()(const Element<K, V> &a, const Element<K, V> &b) const {
+		return a.get_height() > b.get_height();
 	}
 };
 
 template<typename K, typename V>
 class Inner_stack {
 private:
-	std::map<K, std::shared_ptr<Storage<K, V>>> storage_map;
+	std::set<std::shared_ptr<K>, key_comparator> keys;
+	std::map<std::shared_ptr<K>, std::stack<Element<K, V>>, key_comparator> element_map;
+	std::set<Element<K, V>, element_height_comparator> element_set;
 
-	std::set<std::pair<size_t, std::shared_ptr<Storage<K, V>>>> storage_set;
-
-	size_t referenced_storages = 0;
+	bool was_referenced = false;
 	size_t height = 0;
 
 public:
 	Inner_stack() = default;
 
 	Inner_stack(Inner_stack const &inner_stack) {
-		for (const auto &[key, value]: inner_stack.storage_map) {
-			std::shared_ptr<Storage<K, V>> pointer = 
-				std::make_shared<Storage<K, V>>(value);
-			storage_map[key] = pointer;
-			storage_set.insert(std::pair(pointer->height(), pointer));
-		}
+		clear();
 
-		referenced_storages = 0;
-		height = inner_stack.height;
+		for (auto it = inner_stack.element_set.rbegin(); it != inner_stack.element_set.rend(); ++it)
+			push(it->get_key(), it->get_value());
+
+		was_referenced = false;
 	}
 
 
 	void push(K const &key, V const &value) {
+		Rollback_helper rollback(*this);
+
+		std::shared_ptr<K> key_pointer = std::make_shared<K>(key);
+		if (keys.contains(key_pointer))
+			key_pointer = *keys.find(key_pointer);
+		else
+			keys.insert(key_pointer);
+
+		std::shared_ptr<V> value_pointer = std::make_shared<V>(value);
+
+		Element<K, V> element(key_pointer, value_pointer, height);
+
+		if (not element_map.contains(key_pointer))
+			element_map[key_pointer] = std::stack<Element<K, V>>();
+
+		element_map[key_pointer].push(element);
+		element_set.insert(element);
+
 		height++;
+		was_referenced = true;
 
-		if (not storage_map.contains(key))
-			storage_map[key] = std::make_shared<Storage<K, V>>(key);
-
-		std::shared_ptr<Storage<K, V>> &pointer = storage_map[key];
-		storage_set.erase(std::pair(pointer->height(), pointer));
-
-		pointer->push(value, height);
-		storage_set.insert(std::pair(pointer->height(), pointer));
+		rollback.set_commit();
 	}
 
 	void pop() {
+		Rollback_helper rollback(*this);
+
+		Element<K, V> element = *element_set.begin();
+		element_set.erase(element);
+		
+		std::shared_ptr<K> key_pointer = element.key_pointer();
+		element_map[key_pointer].pop();
+
+		if (element_map[key_pointer].empty()) {
+			keys.erase(key_pointer);
+			element_map.erase(key_pointer);
+		}
+
 		height--;
+		was_referenced = true;
 
-		const std::shared_ptr<Storage<K, V>> &pointer = 
-			prev(storage_set.end())->second;
-		bool was_referenced = pointer->is_referenced();
-		storage_set.erase(std::pair(pointer->height(), pointer));
-
-		pointer->pop();
-		bool is_referenced = pointer->is_referenced();
-		storage_set.insert(std::pair(pointer->height(), pointer));
-
-		if (was_referenced and (not is_referenced))
-			referenced_storages--;
+		rollback.set_commit();
 	}
 
 	void pop(K const &key) {
+		Rollback_helper rollback(*this);
+
+		std::shared_ptr<K> key_pointer = std::make_shared<K>(key);
+		Element<K, V> element = element_map[key_pointer].top();
+
+		element_map[key_pointer].pop();
+		element_set.erase(element);
+
+		if (element_map[key_pointer].empty()) {
+			keys.erase(key_pointer);
+			element_map.erase(key_pointer);
+		}
+
 		height--;
+		was_referenced = true;
 
-		const std::shared_ptr<Storage<K, V>> &pointer = storage_map[key];
-		bool was_referenced = pointer->is_referenced();
-		storage_set.erase(std::pair(pointer->height(), pointer));
-
-		pointer->pop();
-		bool is_referenced = pointer->is_referenced();
-		storage_set.insert(std::pair(pointer->height(), pointer));
-
-		if (was_referenced and (not is_referenced))
-			referenced_storages--;
+		rollback.set_commit();
 	}
 
 	std::pair<K const &, V &> front() {
-		const std::shared_ptr<Storage<K, V>> &pointer = 
-			prev(storage_set.end())->second;
-		bool was_referenced = pointer->is_referenced();
+		Element<K, V> element = *element_set.begin();
 
-		std::pair<K const &, V &> result = 
-			{pointer->get_key(), pointer->top()};
-
-		if (not was_referenced)
-			referenced_storages++;
+		std::pair<K const &, V &> result = {element.get_key(), element.get_value()};
+		was_referenced = true;
 
 		return result;
 	}
 
 	std::pair<K const &, V const &> front() const {
-		const std::shared_ptr<Storage<K, V>> &pointer = 
-			prev(storage_set.end())->second;
-		
-		std::pair<K const &, V const &> result = 
-			{pointer->get_key(), pointer->top()};
+		const Element<K, V> element = *element_set.begin();
+
+		std::pair<K const &, V const &> result = {element.get_key(), element.get_value()};
 
 		return result;
 	}
 
 	V & front(K const &key) {
-		std::shared_ptr<Storage<K, V>> &pointer = storage_map[key];
-		bool was_referenced = pointer->is_referenced();
+		std::shared_ptr key_pointer = std::make_shared<K>(key);
+		Element<K, V> element = element_map[key_pointer].top();
 
-		V &result = pointer->top();
+		was_referenced = true;
 
-		if (not was_referenced)
-			referenced_storages++;
-
-		return result;
+		return element.get_value();
 	}
 
 	V const & front(K const &key) const {
-		V const &result;
+		std::shared_ptr key_pointer = std::make_shared<K>(key);
+		Element<K, V> element = element_map[key].top();
 
-		std::shared_ptr<Storage<K, V>> &pointer = storage_map[key];
-
-		result = pointer->top();
-
-		return result;
+		return element.get_value();
 	}
 
 	size_t size() const {
@@ -194,72 +187,63 @@ public:
 	}
 
 	size_t count(K const &key) const {
-		auto it = storage_map.find(key);
-		if (it == storage_map.end())
+		std::shared_ptr key_pointer = std::make_shared<K>(key);
+		auto it = element_map.find(key_pointer);
+
+		if (it == element_map.end())
 			return 0;
-		return it->second->size();
+		return (it->second).size();
 	}
 
 	void clear() {
-		storage_map.clear();
-		storage_set.clear();
+		keys.clear();
+		element_map.clear();
+		element_set.clear();
+		was_referenced = false;
 		height = 0;
-		referenced_storages = 0;
 	}
 
 	bool is_referenced() const {
-		return referenced_storages > 0;
+		return was_referenced;
 	}
 
-	const std::map<K, std::shared_ptr<Storage<K, V>>>& get_storage_map() {
-		return storage_map;
+	const std::set<std::shared_ptr<K>, key_comparator>& get_keys() const {
+		return keys;
 	}
-};
 
-template<typename K, typename V>
-class const_stack_iterator {
 private:
-	using map_it = 
-		typename std::map<K, std::shared_ptr<Storage<K, V>>>
-		::const_iterator;
+class Rollback_helper {
+	private:
+		Inner_stack<K, V>& stack;
+		bool commit = false;
 
-	map_it map_iterator;
+		std::set<std::shared_ptr<K>, key_comparator> old_keys;
+		std::map<std::shared_ptr<K>, std::stack<Element<K, V>>, key_comparator> old_element_map;
+		std::set<Element<K, V>, element_height_comparator> old_element_set;
+		size_t old_height;
 
-public:
-	using iterator_category = std::forward_iterator_tag;
-	using value_type = const K;
-	using pointer = const K*;
-	using reference = const K&;
+	public:
+		Rollback_helper(Inner_stack<K, V>& stack) : stack(stack) {
+			old_keys = stack.keys;
+			old_element_map = stack.element_map;
+			old_element_set = stack.element_set;
+			old_height = stack.height;
+	
+		}
 
-	const_stack_iterator(map_it it)
-		: map_iterator(it) {}
+		~Rollback_helper() noexcept {
+			if (!commit) {
+				stack.keys = std::move(old_keys);
+				stack.element_map = std::move(old_element_map);
+				stack.element_set = std::move(old_element_set);
+				stack.height = old_height;
+			}
+		}
 
-	reference operator*() const {
-		return map_iterator->first;
-	}
-
-	pointer operator->() const {
-		return &(map_iterator->first);
-	}
-
-	const_stack_iterator& operator++() {
-		map_iterator++;
-		return *this;
-	}
-
-	const_stack_iterator operator++(int) {
-		const_stack_iterator iterator = *this;
-		++(*this);
-		return iterator;
-	}
-
-	bool operator==(const const_stack_iterator &other) const {
-		return map_iterator == other.map_iterator;
-	}
-
-	bool operator!=(const const_stack_iterator &other) const {
-		return map_iterator != other.map_iterator;
-	}
+		void set_commit() noexcept {
+			commit = true;
+		}
+	};
 };
 
 namespace cxx {
@@ -296,30 +280,52 @@ public:
 	}
 
 	void push(K const &key, V const &value) {
-		if (not stack_pointer.unique())
-			detach();
+		Detach_rollback_helper detach_helper(stack_pointer);
 
-		stack_pointer->push(key, value);
+		if (not stack_pointer.unique())
+			detach_helper.detach();
+
+		try {
+			stack_pointer->push(key, value);
+			detach_helper.set_commit();
+		} catch (...) {
+			throw;
+		}
 	}
 	
 	void pop() {
 		if (stack_pointer->size() == 0)
 			throw std::invalid_argument("Stack is empty.");
 
-		if (not stack_pointer.unique())
-			detach();
+		Detach_rollback_helper detach_helper(stack_pointer);
 
-		stack_pointer->pop();
+		if (not stack_pointer.unique())
+			detach_helper.detach();
+
+		try {
+			stack_pointer->pop();
+			detach_helper.set_commit();
+		} catch (...) {
+			throw;
+		}
+
 	}
 
 	void pop(K const &key) {
 		if (stack_pointer->count(key) == 0)
 			throw std::invalid_argument("Key not found in stack.");
 
-		if (not stack_pointer.unique())
-			detach();
+		Detach_rollback_helper detach_helper(stack_pointer);
 
-		stack_pointer->pop(key);
+		if (not stack_pointer.unique())
+			detach_helper.detach();
+		
+		try {
+			stack_pointer->pop(key);
+			detach_helper.set_commit();
+		} catch (...) {
+			throw;
+		}
 	}
 
 	std::pair<K const &, V &> front() {
@@ -366,15 +372,85 @@ public:
 	}
 
 public:
-	using const_iterator = const_stack_iterator<K, V>;
+	class const_iterator {
+	private:
+		using set_it = 
+			typename std::set<std::shared_ptr<K>, key_comparator>
+			::const_iterator;
+
+		set_it set_iterator;
+
+	public:
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = const K;
+		using difference_type = ptrdiff_t;
+		using pointer = value_type*;
+		using reference = value_type&;
+
+		const_iterator(set_it it)
+			: set_iterator(it) {}
+
+		reference operator*() const {
+			return **set_iterator;
+		}
+
+		pointer operator->() const {
+			return set_iterator->get;
+		}
+
+		const_iterator& operator++() {
+			set_iterator++;
+			return *this;
+		}
+
+		const_iterator operator++(int) {
+			const_iterator iterator = *this;
+			++(*this);
+			return iterator;
+		}
+
+		bool operator==(const const_iterator &other) const {
+			return set_iterator == other.set_iterator;
+		}
+
+		bool operator!=(const const_iterator &other) const {
+			return set_iterator != other.set_iterator;
+		}
+	};
 
 	const_iterator cbegin() const {
-		return const_iterator(stack_pointer->get_storage_map().cbegin());
+		return const_iterator((stack_pointer->get_keys()).cbegin());
 	}
 
 	const_iterator cend() const {
-		return const_iterator(stack_pointer->get_storage_map().cend());
+		return const_iterator((stack_pointer->get_keys()).cend());
 	}
+
+private:
+	class Detach_rollback_helper {
+	private:
+		std::shared_ptr<Inner_stack<K, V>>& stack_pointer;
+		std::shared_ptr<Inner_stack<K, V>> original_stack;
+		bool commit = false;
+
+	public:
+		Detach_rollback_helper(std::shared_ptr<Inner_stack<K, V>>& sptr) 
+			: stack_pointer(sptr), original_stack(sptr) {}
+
+		void detach() {
+			stack_pointer = std::make_shared<Inner_stack<K, V>>(*original_stack);
+		}
+
+		void set_commit() {
+			commit = true;
+		}
+
+		~Detach_rollback_helper() noexcept {
+			if (!commit) {
+				stack_pointer = original_stack;
+			}
+		}
+	};
 };
 }
 
